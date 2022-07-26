727bbf45a1125dbb764f4f87f36fd627d76b3436
DamnStation <radarawow@gmail.com>
2022-07-26 22:04:35 +0300

    Added -c Api/ "index" Get all reservations

---
+++ pages/api/reservation/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connect } from "../../../utils/database";
+
+//Get all reservations
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { db } = await connect();
+    const reservations = await db.collection("reservations").find({}).toArray();
+    res.status(200);
+    res.json({ reservations });
+  } catch (error) {
+    res.status(500);
+    res.json({ error: "Unable to get reservations" });
+  }
+};
